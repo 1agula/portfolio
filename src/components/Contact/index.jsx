939046b8c1d22ef6ca7d87cea61fdffe22b39741
sudopain <sudopain@gmail.com>
2021-12-13 22:04:35 +0900

    create Testimonials and Contact components

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -1,10 +1,26 @@
+import { useState } from "react";
 import "./index.scss";
 
 export default function Contact() {
+  const [message, setMessage] = useState(false);
+  const handleSubimit = (e) => {
+    e.preventDefault();
+    setMessage(true);
+  };
   return (
     <div className="contact" id="contact">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam cupiditate,
-      velit fugit debitis saepe rerum molestias sit porro in sapiente!
+      <div className="left">
+        <img src="assets/shake.svg" alt="shake" />
+      </div>
+      <div className="right">
+        <h2>Contact</h2>
+        <form onSubmit={handleSubimit} type="text">
+          <input type="text" placeholder="Email" />
+          <textarea placeholder="Message"></textarea>
+          <button type="submit">Send</button>
+          {message && <span>Thanks, I'll reply ASAP :)</span>}
+        </form>
+      </div>
     </div>
   );
 }
